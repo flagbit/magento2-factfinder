d78c9f4ec9a27d0f1b4952654975d1b17b6de303
thakilla <essig@flagbit.de>
2015-08-21 01:40:04 +0300

    Added controller which is used as import link in FACTFinder

---
+++ Controller/Export/Get.php
@@ -0,0 +1,46 @@
+<?php
+namespace Flagbit\FACTFinder\Controller\Export;
+
+use \Magento\Framework\App\Filesystem\DirectoryList;
+use \Magento\Framework\Controller\ResultFactory;
+
+class Get extends \Flagbit\FACTFinder\Controller\Export
+{
+    /**
+     * @var \Magento\Framework\Filesystem
+     */
+    protected $_filesystem;
+
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     */
+    public function __construct(\Magento\Framework\App\Action\Context $context)
+    {
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+
+        $this->_filesystem = $objectManager->get('\Magento\Framework\Filesystem');
+
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $fileName = '/factfinder/products.csv';
+        $filePath = $this->_filesystem->getDirectoryRead(DirectoryList::VAR_DIR);
+
+        if (!file_exists($filePath->getAbsolutePath() . $fileName)) {
+            /** @var \Magento\Framework\Controller\Result\Raw $resultRaw */
+            $resultRaw = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+            $resultRaw->setContents('No export file was generated yet. Please trigger an export through magento\'s backend.');
+            return $resultRaw;
+        }
+
+        $fileContent = $filePath->readFile($fileName);
+
+        $this->_response->setHeader('Content-type', 'text/plain', true);
+
+        $resultRaw = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        $resultRaw->setContents($fileContent);
+        return $resultRaw;
+    }
+}
